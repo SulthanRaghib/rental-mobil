f553b8191bc04d8859eff4c8972c8c77c1b674b2
SulthanRaghib <raghib.smp@gmail.com>
2023-09-24 17:11:49 +0700

    delete duplicate field table

--- app/Http/Controllers/PembayaranController.php
+++ app/Http/Controllers/PembayaranController.php
@@ -9,4 +9,40 @@ use Illuminate\Support\Str;
 
 class PembayaranController extends Controller
 {
+    public function create(Request $request)
+    {
+        $params = array(
+            'transaction_details' => array(
+                'order_id' => str::uuid(),
+                'total_bayar' => $request->gross_amount,
+            ),
+            'customer_details' => array(
+                'first_name' => $request->first_name,
+                'last_name' => $request->last_name,
+                'email' => $request->email,
+                'phone' => $request->phone,
+            ),
+            'item_details' => array(
+                array(
+                    'id' => $request->id,
+                    'price' => $request->price,
+                    'quantity' => $request->quantity,
+                    'name' => $request->name,
+                ),
+            ),
+            'enabled_payments' => array(
+                'credit_card', 'bca_va', 'bni_va', 'bri_va'
+            ),
+        );
+
+        $auth = base64_encode(env('MIDTRANS_SERVER_KEY'));
+
+        $response = Http::withHeader([
+            'Content-Type' => 'application/json',
+            'Authorization' => 'Basic ' . $auth,
+        ])->post('https://api.sandbox.midtrans.com/snap/v1/transactions', $params);
+
+        $payment = new Pembayaran;
+        $payment->order_id = $response['order_id'];
+    }
 }
--- database/migrations/2023_09_24_093131_create_pembayaran_table.php
+++ database/migrations/2023_09_24_093131_create_pembayaran_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('pembayaran', function (Blueprint $table) {
             $table->id();
-            $table->string('sewa_id');
             $table->string('status');
             $table->string('link_pembayaran');
             $table->foreignId('sewa_id');
